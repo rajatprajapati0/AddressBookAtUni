d2eed6a603a82aeb4b586bfdd578369e557ab257
Rajat <rajatprajapati09@gmail.com>
2023-10-18 09:21:13 +0530

    [AdreessBook welcome msg]

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+    static
+    {
+        System.out.println("Welcome in Address Book program");
+    }
+    public static void main(String[] args)
+    {
+
+
+    }
+
+
+/*
+Displaying
+Welcome to Address Book
+Program in
+AddressBookMain class on
+ */
+}
